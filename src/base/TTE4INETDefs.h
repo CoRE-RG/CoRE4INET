f7c8edf0c07e2d44c38ce52e7fd710f50076e075
steinb_i <steinb_i@00301150-2f75-0410-9ab1-1540baa28df1>
2011-08-02 17:01:22 +0000

    Various improvements in the documentation
    git-svn-id: http://svn.cpt.haw-hamburg.de/svn/RTethernet/source/simulation/TTEthernetModelV2/trunk/TTEthernetModelV2@2129 00301150-2f75-0410-9ab1-1540baa28df1
    
    Former-commit-id: 16099c8dcf8d360875f4648c0be9241c80907726

---
+++ src/base/TTE4INETDefs.h
@@ -0,0 +1,16 @@
+#ifndef __TTE4INET_TTE4INETDEFS_H
+#define __TTE4INET_TTE4INETDEFS_H
+
+//
+// General definitions.
+//
+
+#include <omnetpp.h>
+
+#if OMNETPP_VERSION < 0x0402
+#  error At least OMNeT++/OMNEST version 4.2 required
+#endif
+
+#endif
+
+#define TTE4INET_VERSION 0x0200
---
+++ src/index.h
@@ -0,0 +1,14 @@
+// This file is used only during the doxygen documentation generation.
+
+/**
+ * @page whatsnew What's New
+ * @verbinclude WHATSNEW
+ *
+ * @page readme Readme
+ * @verbinclude README
+ *
+ * @page install Install help
+ * @verbinclude INSTALL
+ */
+
+
