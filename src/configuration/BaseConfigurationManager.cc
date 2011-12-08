38b47e96f3df78584b98d272c787bc8a56672d01
steinbach <till.steinbach@informatik.haw-hamburg.de>
2011-12-08 14:52:17 +0100

    Documentation update
    Former-commit-id: a4defa7994233d8c8c6ed7649aad5a36c1a4793d

---
+++ src/configuration/BaseConfigurationManager.cc
@@ -0,0 +1,35 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "BaseConfigurationManager.h"
+
+namespace TTEthernetModel {
+
+Define_Module(BaseConfigurationManager);
+
+void BaseConfigurationManager::initialize()
+{
+    //Generate start message to delete module
+    scheduleAt(simTime(), new cMessage("Delete Module"));
+}
+
+void BaseConfigurationManager::handleMessage(cMessage *msg)
+{
+    //delete module on first message, it is not needed anymore;
+    delete msg;
+    deleteModule();
+}
+
+} //namespace
---
+++ src/configuration/BaseConfigurationManager.h
@@ -0,0 +1,45 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __TTETHERNETMODEL_BASECONFIGURATIONMANAGER_H_
+#define __TTETHERNETMODEL_BASECONFIGURATIONMANAGER_H_
+
+#include <omnetpp.h>
+
+namespace TTEthernetModel {
+
+/**
+ * @brief Dummy module for a ConfigurationManager that has no configuration.
+ * If you use this BaseConfigurationManager you must configure the node manually
+ *
+ * The module is deleted after simulation start
+ *
+ */
+class BaseConfigurationManager : public cSimpleModule
+{
+  protected:
+    /**
+     * @brief Is called in initialization phase. The module creates a message for self destruction
+     */
+    virtual void initialize();
+    /**
+     * @brief Destroys the module after reception of a message
+     */
+    virtual void handleMessage(cMessage *msg);
+};
+
+} //namespace
+
+#endif
