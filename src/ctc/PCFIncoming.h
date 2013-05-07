47aeb8010954713c06c9e87c9f7d4957efd8c311
Till Steinbach <till.steinbach@informatik.haw-hamburg.de>
2013-05-07 18:39:09 +0200

    Some changes to PCF related structures
    Former-commit-id: 8bf226a1aa9f0fcbb4b7a5e5c1f6472e3f7a2c99

--- src/ctc/PCFIncoming.h
+++ src/ctc/PCFIncoming.h
@@ -3,51 +3,48 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
-#ifndef __TTETHERNETMODELV2_RCINCOMING_H_
-#define __TTETHERNETMODELV2_RCINCOMING_H_
+//
+#ifndef __TTETHERNETMODELV2_PCFINCOMING_H_
+#define __TTETHERNETMODELV2_PCFINCOMING_H_
 
 #include <omnetpp.h>
 #include <Incoming.h>
+
+#include "PCFrame_m.h"
 
 namespace TTEthernetModel {
 
 /**
  * @brief Class for the critical traffic conformance check of
- * rate-constrained traffic.
+ * pcf traffic.
  *
- * This class checks whether incoming traffic was conform with the configured bag.
+ * This class checks whether incoming traffic was conform with the configured pcf type.
  * Conform messages are forwarded from in to out
  *
  * @sa Incoming
  *
- * @author Till Steinbach
+ * @author Lazar Todorov, Till Steinbach
  */
-class RCIncoming : public Incoming
+class PCFIncoming:public Incoming
 {
     private:
         /**
-         * Saves the tickcount of the last message
+         * @brief caches pcfType parameter
          */
-        uint64_t lastArrived;
+        PCFType pcfType;
 
-        /**
-         * @brief caches bag parameter
-         */
-        uint64_t bag;
     protected:
         /**
          * @brief Forwards messages arriving on in-gate to out-gate checks
-         * conformance with configured bag.
+         * conformance with configured pcf type.
          *
          * @param msg the incoming message
          */
@@ -59,13 +56,20 @@ class RCIncoming : public Incoming
          * @param parname Name of the changed parameter or NULL if multiple parameter changed.
          */
         virtual void handleParameterChange(const char* parname);
+
+        /**
+         * @brief Initialization of the module
+         */
+        virtual void initialize();
+
     public:
         /**
          * @brief Constructor.
          */
-        RCIncoming();
+        PCFIncoming();
+
 };
 
-} //namespace
+}//namespace
 
 #endif
