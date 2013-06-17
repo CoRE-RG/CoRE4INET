ba163cde9c93139fd488afa7900c5a87c3cfbfdd
Steinbach <steinbach.till@gmx.de>
2013-06-17 17:39:27 +0200

    Added ned seconds to ticks conversion function

--- src/utilities/HelperFunctions.cc
+++ src/utilities/HelperFunctions.cc
@@ -79,3 +79,14 @@ uint64_t secondsToTransparentClock(simtime_t seconds){
 uint64_t transparentClockToTicks(uint64_t transparentClock, simtime_t tick){
     return transparentClock/secondsToTransparentClock(tick);
 }
+
+static cNEDValue ned_sec2tick(cComponent *context, cNEDValue argv[], int argc)
+{
+    cModule* module = findModuleWhereverInNode("tteScheduler",context->getParentModule());
+    if(!module)
+        throw cRuntimeError("ned_sec2tick(): cannot determine tick length");
+    long ticks = argv[0].doubleValueInUnit("s")/module->par("tick").doubleValue();
+    argv[0].set(ticks, "tick");
+    return argv[0];
+}
+Define_NED_Function(ned_sec2tick, "quantity sec2tick(quantity seconds)");
