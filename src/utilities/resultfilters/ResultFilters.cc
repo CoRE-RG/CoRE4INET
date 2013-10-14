0500cd7c4f74db51b6879f27d00a37c3e6664ab7
Till Steinbach <till.steinbach@informatik.haw-hamburg.de>
2013-10-14 15:46:53 +0200

    new result filters

--- src/utilities/resultfilters/ResultFilters.cc
+++ src/utilities/resultfilters/ResultFilters.cc
@@ -1,3 +1,17 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 #include "ResultFilters.h"
 
 #include <cmessage.h>
@@ -8,7 +22,7 @@ Register_ResultFilter("innerMessage", InnerMessageFilter);
 
 void InnerMessageFilter::receiveSignal(cResultFilter *prev, simtime_t_cref t, cObject *object)
 {
-    if (dynamic_cast<cMessage *>(object))
+    if (dynamic_cast<cPacket *>(object))
     {
         cPacket *innerPacket = (cPacket *)object;
         cPacket *tmpPacket;
@@ -18,4 +32,56 @@ void InnerMessageFilter::receiveSignal(cResultFilter *prev, simtime_t_cref t, cO
         fire(this, t, innerPacket);
         delete(innerPacket);
     }
+}
+
+FloatingIntervalFilter::FloatingIntervalFilter(){
+    interval = SimTime(1);
+}
+
+bool FloatingIntervalFilter::process(simtime_t &t, double &value){
+    //first add new value to interval, give hint for faster execution
+    if(inInterval.size()>0){
+        inInterval.insert(--inInterval.end(), std::pair<simtime_t, double>(t, value));
+    }
+    else{
+        inInterval[t]=value;
+    }
+    //erase old values
+    inInterval.erase(inInterval.begin(), inInterval.lower_bound((t-interval)));
+    return true;
+}
+
+Register_ResultFilter("floatingIntervalCount", FloatingIntervalCountFilter);
+
+bool FloatingIntervalCountFilter::process(simtime_t &t, double &value){
+    FloatingIntervalFilter::process(t, value);
+    //return size
+    value = inInterval.size();
+    return true;
+}
+
+Register_ResultFilter("floatingIntervalSum", FloatingIntervalSumFilter);
+
+bool FloatingIntervalSumFilter::process(simtime_t &t, double &value){
+    FloatingIntervalFilter::process(t, value);
+    //build sum
+    double newValue = 0;
+    for(std::map<simtime_t, double>::iterator it2 = inInterval.begin(); it2 != inInterval.end(); ++it2){
+        newValue+=(*it2).second;
+    }
+    value = newValue;
+    return true;
+}
+
+Register_ResultFilter("floatingIntervalAvg", FloatingIntervalAvgFilter);
+
+bool FloatingIntervalAvgFilter::process(simtime_t &t, double &value){
+    FloatingIntervalFilter::process(t, value);
+    //build average
+    double newValue = 0;
+    for(std::map<simtime_t, double>::iterator it2 = inInterval.begin(); it2 != inInterval.end(); ++it2){
+        newValue+=(*it2).second;
+    }
+    value = (newValue / inInterval.size());
+    return true;
 }
--- src/utilities/resultfilters/ResultFilters.h
+++ src/utilities/resultfilters/ResultFilters.h
@@ -1,3 +1,17 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 #ifndef __TTE4INET_RESULTFILTERS_H
 #define __TTE4INET_RESULTFILTERS_H
 
@@ -6,13 +20,40 @@
 namespace TTEthernetModel{
 
 /**
- * Filter that expects a cMessage and outputs its age in seconds
- * (t - msg->getCreationTime()).
+ * Filter that expects a cPacket and outputs its encapsulated cPacket
  */
 class InnerMessageFilter : public cObjectResultFilter
 {
     public:
         virtual void receiveSignal(cResultFilter *prev, simtime_t_cref t, cObject *object);
+};
+
+class FloatingIntervalFilter : public cNumericResultFilter{
+    protected:
+        simtime_t interval;
+        std::map<simtime_t, double> inInterval;
+    protected:
+        virtual bool process(simtime_t &t, double& value);
+    public:
+        FloatingIntervalFilter();
+};
+
+class FloatingIntervalCountFilter : public FloatingIntervalFilter
+{
+    protected:
+        virtual bool process(simtime_t &t, double& value);
+};
+
+class FloatingIntervalSumFilter : public FloatingIntervalFilter
+{
+    protected:
+        virtual bool process(simtime_t &t, double& value);
+};
+
+class FloatingIntervalAvgFilter : public FloatingIntervalFilter
+{
+    protected:
+        virtual bool process(simtime_t &t, double& value);
 };
 
 }
