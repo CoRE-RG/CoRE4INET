b9ea284924f0449015eb4837b8f2490eb3799108
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2015-05-12 16:59:49 +0200

    added RCBurstTrafficSource

---
+++ src/applications/trafficsource/AS6802/CoRE4INET_RCBurstTrafficSourceApp.h
@@ -0,0 +1,75 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef RCBURSTTRAFFICSOURCEAPP_H_
+#define RCBURSTTRAFFICSOURCEAPP_H_
+
+//CoRE4INET
+#include "CoRE4INET_CTTrafficSourceAppBase.h"
+#include "CoRE4INET_Timed.h"
+
+namespace CoRE4INET {
+
+/**
+ * @brief Traffic-Generator for RC-Traffic with burst behavior.
+ *
+ *
+ * @sa ApplicationBase
+ * @ingroup Applications AS6802
+ *
+ * @author Philipp Meyer
+ */
+class RCBurstTrafficSourceApp : public virtual CoRE4INET::CTTrafficSourceAppBase, public virtual CoRE4INET::Timed
+{
+    /**
+     * @brief Caches interval parameter
+     */
+    double interval;
+    /**
+     * @brief Caches burstInterval parameter
+     */
+    double burstInterval;
+    /**
+     * @brief Counts the number of frames that are send until now
+     */
+    unsigned int framesBurstedCnt;
+
+
+    protected:
+        /**
+         * @brief Initialization of the module. Sends activator message
+         */
+        virtual void initialize() override;
+        /**
+         * @brief Handles message generation
+         */
+        virtual void handleMessage(cMessage *msg) override;
+
+        /**
+         * @brief Indicates a parameter has changed.
+         *
+         * @param parname Name of the changed parameter or nullptr if multiple parameter changed.
+         */
+        virtual void handleParameterChange(const char* parname) override;
+    public:
+        /**
+         * @brief constructor for RCBurstTrafficSourceApp
+         */
+        RCBurstTrafficSourceApp();
+};
+
+} //namespace
+
+#endif /* RCBURSTTRAFFICSOURCEAPP_H_ */
