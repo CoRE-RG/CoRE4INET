9539ec154b918a95eea39ff0c2936600aa588541
steinbach <till.steinbach@informatik.haw-hamburg.de>
2015-07-08 11:04:19 +0200

    First part of IEEE8021Q extension for inet integration

---
+++ src/networklayer/inet/IEEE8021Q/CoRE4INET_IPv4oIEEE8021Q.h
@@ -0,0 +1,68 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef CORE4INET_IPV4OIEEE8021Q_H_
+#define CORE4INET_IPV4OIEEE8021Q_H_
+
+//==============================================================================
+
+#include "CoRE4INET_IPv4oREBase.h"
+
+#include "CoRE4INET_BGBuffer.h"
+
+//==============================================================================
+
+namespace CoRE4INET {
+
+//==============================================================================
+
+template<class Base>
+class IPv4oIEEE8021Q : public Base {
+
+
+public:
+
+    IPv4oIEEE8021Q();
+    virtual ~IPv4oIEEE8021Q();
+
+    virtual void initialize(int stage) override;
+    virtual void sendPacketToNIC(cPacket *packet, const inet::InterfaceEntry *ie);
+    virtual void configureFilters(cXMLElement *config);
+    virtual void handleMessage(cMessage* msg) override;
+
+    /**
+     * Encapsulates packet in RC frame and sends to each destination buffers.
+     */
+    virtual void sendPacketToBuffers(cPacket *packet, const inet::InterfaceEntry *ie, std::list<IPoREFilter*> &filters);
+
+    /**
+     * Encapsulates packet in RC Frame and sends to destination Buffers.
+     */
+    void sendIEEE8021QFrame(cPacket* packet, const inet::InterfaceEntry* ie, const IPoREFilter* filter);
+
+
+};
+
+//==============================================================================
+
+} /* namespace CoRE4INET */
+
+//==============================================================================
+
+#include "CoRE4INET_IPv4oRC.cc"
+
+//==============================================================================
+
+#endif /* CORE4INET_IPV4ORC_H_ */
