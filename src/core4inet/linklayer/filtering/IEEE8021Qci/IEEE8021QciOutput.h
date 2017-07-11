289e6b40e07a83250ff522e8f921cb30812a205d
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2017-07-11 15:20:15 +0200

    Add IEEE8021Qci filtering structure

---
+++ src/core4inet/linklayer/filtering/IEEE8021Qci/IEEE8021QciOutput.h
@@ -0,0 +1,37 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CORE4INET_IEEE8021QCIOUTPUT_H_
+#define __CORE4INET_IEEE8021QCIOUTPUT_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+namespace CoRE4INET {
+
+/**
+ * TODO - Generated class
+ */
+class IEEE8021QciOutput : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+} //namespace
+
+#endif
