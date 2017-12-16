764728a3665ba2dc88a61103e022a3bdea70baca
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2017-12-16 15:51:43 +0100

    Add TTEAVBEtherHostMultiPorts Ethernet node

---
+++ src/core4inet/linklayer/ethernet/avb/SRPEtherEncapAdapter.h
@@ -0,0 +1,43 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CORE4INET_SRPETHERENCAPADAPTER_H_
+#define __CORE4INET_SRPETHERENCAPADAPTER_H_
+
+#include <omnetpp.h>
+//Auto-generated Messages
+#include "core4inet/linklayer/contract/ExtendedIeee802Ctrl_m.h"
+#include "core4inet/linklayer/ethernet/avb/SRPFrame_m.h"
+
+using namespace omnetpp;
+
+namespace CoRE4INET {
+
+/**
+ * TODO - Generated class
+ */
+class SRPEtherEncapAdapter : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+
+  private:
+    size_t portCount;
+};
+
+} //namespace
+
+#endif
