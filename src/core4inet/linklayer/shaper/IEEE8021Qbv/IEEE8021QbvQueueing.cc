843c97f7b92d467a5cffaf34b2ee5cc36cd8602e
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2019-05-14 15:32:21 +0200

    Add IEEE 802.1Qbv queueing implementation

---
+++ src/core4inet/linklayer/shaper/IEEE8021Qbv/IEEE8021QbvQueueing.cc
@@ -0,0 +1,98 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "IEEE8021QbvQueueing.h"
+
+//std
+#include <algorithm>
+#include <limits>
+//CoRE4INET
+#include "core4inet/base/CoRE4INET_Defs.h"
+#include "core4inet/utilities/ConfigFunctions.h"
+//Auto-generated Messages
+#include "core4inet/linklayer/ethernet/base/EtherFrameWithQTag_m.h"
+
+namespace CoRE4INET {
+
+Define_Module(IEEE8021QbvQueueing);
+
+void IEEE8021QbvQueueing::initialize()
+{
+    this->handleParameterChange(nullptr);
+}
+
+void IEEE8021QbvQueueing::handleMessage(cMessage *msg)
+{
+    if (msg->arrivedOn("in"))
+    {
+        if (EthernetIIFrameWithQTag* qframe = dynamic_cast<EthernetIIFrameWithQTag*>(msg))
+        {
+            //VLAN untag if requested
+            if (this->untaggedVID && this->untaggedVID == qframe->getVID())
+            {
+                qframe->setVID(0);
+            }
+            //VLAN check if port is tagged with VLAN
+            bool found = false;
+            for (std::vector<int>::iterator vid = this->taggedVIDs.begin(); vid != this->taggedVIDs.end(); ++vid)
+            {
+                //Shortcut due to sorted vector
+                if ((*vid) > qframe->getVID())
+                {
+                    break;
+                }
+                if ((*vid) == qframe->getVID())
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                delete qframe;
+                return;
+            }
+            this->send(qframe, "out", qframe->getPcp());
+        }
+        else
+        {
+            this->send(msg, "out", this->defaultPCP);
+        }
+        // TODO: Trigger Selection
+    }
+}
+
+void IEEE8021QbvQueueing::handleParameterChange(const char* parname)
+{
+    if (!parname || !strcmp(parname, "untaggedVID"))
+    {
+        this->untaggedVID = static_cast<uint16_t>(parameterULongCheckRange(par("untaggedVID"), 0, MAX_VLAN_NUMBER));
+    }
+    if (!parname || !strcmp(parname, "taggedVIDs"))
+    {
+        this->taggedVIDs = cStringTokenizer(par("taggedVIDs"), ",").asIntVector();
+        std::sort(taggedVIDs.begin(), taggedVIDs.end());
+    }
+    if (!parname || !strcmp(parname, "numPCP"))
+    {
+        this->numPCP = parameterULongCheckRange(par("numPCP"), 1, std::numeric_limits<unsigned int>::max());
+    }
+    if (!parname || !strcmp(parname, "defaultPCP"))
+    {
+        this->defaultPCP = parameterULongCheckRange(par("defaultPCP"), 0, this->numPCP-1);
+    }
+}
+
+} //namespace
---
+++ src/core4inet/linklayer/shaper/IEEE8021Qbv/IEEE8021QbvQueueing.h
@@ -0,0 +1,51 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CORE4INET_IEEE8021QBVQUEUEING_H_
+#define __CORE4INET_IEEE8021QBVQUEUEING_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+namespace CoRE4INET {
+
+/**
+ * @author Philipp Meyer
+ */
+class IEEE8021QbvQueueing : public cSimpleModule
+{
+  private:
+    /**
+     * @brief Untagged VLAN.
+     * Untagged incoming frames get tagged with this VLAN. Outgoing frames with this VLAN get untagged.
+     */
+    uint16_t untaggedVID;
+    /**
+     * @brief Tagged VLANs.
+     * Only outgoing frames with one of the VLANs in this list are transmitted. Default is "0" to allow for untagged frames
+     */
+    std::vector<int> taggedVIDs;
+    unsigned int numPCP;
+    unsigned int defaultPCP;
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+    virtual void handleParameterChange(const char* parname);
+};
+
+} //namespace
+
+#endif
