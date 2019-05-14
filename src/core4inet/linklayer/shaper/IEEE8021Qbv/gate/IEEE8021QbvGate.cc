935effc05b733d62867c8157d3bd2bf781a9678b
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2019-05-14 15:46:24 +0200

    Add IEEE 802.1Qbv gate implementation

---
+++ src/core4inet/linklayer/shaper/IEEE8021Qbv/gate/IEEE8021QbvGate.cc
@@ -0,0 +1,59 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "IEEE8021QbvGate.h"
+
+namespace CoRE4INET {
+
+Define_Module(IEEE8021QbvGate);
+
+void IEEE8021QbvGate::initialize()
+{
+    this->handleParameterChange(nullptr);
+    WATCH(this->state);
+}
+
+void IEEE8021QbvGate::handleParameterChange(const char *parname)
+{
+    if (!parname || !strcmp(parname, "state"))
+    {
+        if (!strcmp(par("state").stringValue(), "O"))
+        {
+            this->state = this->State::OPEN;
+        }
+        else if (!strcmp(par("state").stringValue(), "C"))
+        {
+            this->state = this->State::CLOSE;
+        }
+        else
+        {
+            throw cRuntimeError("Parameter state of %s is %s and is only allowed to be O or C", getFullPath().c_str(), par("state").stringValue());
+        }
+    }
+}
+
+void IEEE8021QbvGate::refreshDisplay() const
+{
+    if (this->state == this->State::OPEN)
+    {
+        this->getDisplayString().setTagArg("b",3,"green");
+    }
+    else if (this->state == this->State::CLOSE)
+    {
+        this->getDisplayString().setTagArg("b",3,"red");
+    }
+}
+
+} //namespace
---
+++ src/core4inet/linklayer/shaper/IEEE8021Qbv/gate/IEEE8021QbvGate.h
@@ -0,0 +1,65 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CORE4INET_IEEE8021QBVGATE_H_
+#define __CORE4INET_IEEE8021QBVGATE_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+namespace CoRE4INET {
+
+/**
+ * @author Philipp Meyer
+ */
+class IEEE8021QbvGate : public cSimpleModule
+{
+  public:
+    /**
+     * @brief State of the IEEE8021QbvGate
+     */
+    enum State
+    {
+      CLOSE = 0,//!< CLOSE
+      OPEN = 1  //!< OPEN
+    };
+
+  private:
+    /**
+     * @brief Current state of the IEEE8021QbvGate.
+     */
+    State state;
+
+  protected:
+    /**
+     * @brief Initializes the gate
+     */
+    virtual void initialize();
+    /**
+     * @brief Indicates a parameter has changed.
+     *
+     * @param parname Name of the changed parameter or nullptr if multiple parameter changed.
+     */
+    virtual void handleParameterChange(const char *parname);
+    /**
+     * @brief Components that contain visualization-related code are expected to override refreshDisplay()
+     */
+    virtual void refreshDisplay() const;
+};
+
+} //namespace
+
+#endif
