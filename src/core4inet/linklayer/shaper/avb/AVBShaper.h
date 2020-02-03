0d6cbcebc55bccb67ac9b039c031e318a3c5964c
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2020-02-03 14:45:36 +0100

    Minor refinements

--- src/core4inet/linklayer/shaper/avb/AVBShaper.h
+++ src/core4inet/linklayer/shaper/avb/AVBShaper.h
@@ -1,9 +1,17 @@
-/*
- * CoRE4INET_AVBShaper.h
- *
- *  Created on: 30.01.2015
- *      Author: Philipp
- */
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 
 #ifndef CORE4INET_AVBSHAPER_H_
 #define CORE4INET_AVBSHAPER_H_
@@ -11,7 +19,6 @@
 //CoRE4INET
 #include "core4inet/base/avb/AVBDefs_m.h"
 #include "core4inet/buffer/avb/AVBBuffer.h"
-
 //Auto-generated messages
 #include "core4inet/linklayer/ethernet/avb/AVBFrame_m.h"
 
