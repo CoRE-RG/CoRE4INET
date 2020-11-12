5c799b387032b06fc7143d71054e2f9c3510a9c0
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2020-11-12 11:22:26 +0100

    Add Reordering and Timing modules

---
+++ src/core4inet/linklayer/corruption/ieee8021qbv/Reordering.h
@@ -0,0 +1,40 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __CORE4INET_REORDERING_H_
+#define __CORE4INET_REORDERING_H_
+
+//OMNeT++
+#include <omnetpp.h>
+//CoRE4INET
+#include "core4inet/linklayer/shaper/IEEE8021Qbv/selection/IEEE8021QbvSelection.h"
+
+using namespace omnetpp;
+
+namespace CoRE4INET {
+
+/**
+ * TODO - Generated class
+ */
+class Reordering : public virtual IEEE8021QbvSelection
+{
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+} //namespace
+
+#endif
