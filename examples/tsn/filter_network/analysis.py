ef0daae62778321f647474854ce7adf09f33a39d
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2021-04-09 12:59:15 +0200

    Add tdma loss detection analysis

--- examples/tsn/filter_network/analysis.py
+++ examples/tsn/filter_network/analysis.py
@@ -69,15 +69,30 @@ for csvresultfile in csvresultfiles:
         'vecvalue': parse_ndarray})
     corruptionsPerformedVectors = csvresult[(csvresult.type=="vector") & (csvresult.name=="corruptionsPerformed:vector")]
     if (len(corruptionsPerformedVectors) > 0):
-        corruptionsPerformedVecTimes = csvresult[(csvresult.type=="vector") & (csvresult.name=="corruptionsPerformed:vector")].iloc[0].vectime
+        corruptionsPerformedVecTimes = corruptionsPerformedVectors.iloc[0].vectime
+        zeroFramesPassedOpenGateTDMAVecTimes = []
+        zeroFramesPassedOpenGateCBSVecTimes = []
+        zeroFramesPassedOpenGateBEVecTimes = []
         dropVectors = csvresult[(csvresult.type=="vector") & (csvresult.name=="framesDropped:vector")]
+        zeroFramesPassedOpenGateTDMAVectors = csvresult[(csvresult.type=="vector") & (csvresult.module=="filter_network.switch1.phy[0].filtering.streamGate[0]") & (csvresult.name=="zeroFramesPassedInOpenPhases:vector")]
+        zeroFramesPassedOpenGateCBSVectors = csvresult[(csvresult.type=="vector") & (csvresult.module=="filter_network.switch1.phy[0].filtering.streamGate[1]") & (csvresult.name=="zeroFramesPassedInOpenPhases:vector")]
+        zeroFramesPassedOpenGateBEVectors = csvresult[(csvresult.type=="vector") & (csvresult.module=="filter_network.switch1.phy[0].filtering.streamGate[2]") & (csvresult.name=="zeroFramesPassedInOpenPhases:vector")]
+        if (len(zeroFramesPassedOpenGateTDMAVectors) > 0):
+            zeroFramesPassedOpenGateTDMAVecTimes = zeroFramesPassedOpenGateTDMAVectors.iloc[0].vectime
+        if (len(zeroFramesPassedOpenGateCBSVectors) > 0):
+            zeroFramesPassedOpenGateCBSVecTimes = zeroFramesPassedOpenGateCBSVectors.iloc[0].vectime
+        if (len(zeroFramesPassedOpenGateBEVectors) > 0):
+            zeroFramesPassedOpenGateBEVecTimes = zeroFramesPassedOpenGateBEVectors.iloc[0].vectime
         corruptionCount = 0
         dropCount = 0
+        lossCount = 0
         singleDropCount = 0
         multiDropCount = 0
-        singleDetectionCount = 0
-        multiDetectionCount = 0
-        detectionCount = 0
+        singleDropDetectionCount = 0
+        multiDropDetectionCount = 0
+        dropDetectionCount = 0
+        lossDetectionCount = 0
+        combiDetectionCount = 0
         for corruptionTime in corruptionsPerformedVecTimes:
             corruptionCount += 1
             drops = 0       
@@ -89,44 +104,84 @@ for csvresultfile in csvresultfiles:
                         break
                     else:
                         drops += 1
+            losses = 0
+            for zeroFramesPassedOpenGateTDMAVecTime in zeroFramesPassedOpenGateTDMAVecTimes:
+                if zeroFramesPassedOpenGateTDMAVecTime < corruptionTime:
+                    continue
+                elif zeroFramesPassedOpenGateTDMAVecTime > (corruptionTime + MAX_DETECTION_WINDOW_SECONDS):
+                    break
+                else:
+                    losses += 1
             if drops > 0:
                 dropCount += drops
-                detectionCount += 1
+                dropDetectionCount += 1
                 if drops == 1:
                     singleDropCount += drops
-                    singleDetectionCount += 1
+                    singleDropDetectionCount += 1
                 elif drops > 1:
                     multiDropCount += drops
-                    multiDetectionCount += 1
+                    multiDropDetectionCount += 1
+            if losses > 0:
+                lossCount += losses
+                lossDetectionCount += 1
+            if drops > 0 or losses > 0:
+                combiDetectionCount += 1
         results[csvresultfilename] = {}
         results[csvresultfilename]["corruptions"] = corruptionCount
         results[csvresultfilename]["drops"] = dropCount
+        results[csvresultfilename]["losses"] = lossCount
         results[csvresultfilename]["singleDrops"] = singleDropCount
         results[csvresultfilename]["multiDrops"] = multiDropCount
-        results[csvresultfilename]["detections"] = detectionCount
-        results[csvresultfilename]["singleDetections"] = singleDetectionCount
-        results[csvresultfilename]["multiDetections"] = multiDetectionCount
-        if multiDetectionCount > 0:
-            results[csvresultfilename]["multiDropsPerMultiDetection"] = multiDropCount / multiDetectionCount
-        results[csvresultfilename]["TruePositives"] = detectionCount
-        results[csvresultfilename]["FalseNegatives"] = corruptionCount - detectionCount
-        results[csvresultfilename]["Recall"] = detectionCount / (detectionCount + (corruptionCount - detectionCount))
+        results[csvresultfilename]["dropDetections"] = dropDetectionCount
+        results[csvresultfilename]["lossDetections"] = lossDetectionCount
+        results[csvresultfilename]["combiDetections"] = combiDetectionCount
+        results[csvresultfilename]["singleDropDetections"] = singleDropDetectionCount
+        results[csvresultfilename]["multiDropDetections"] = multiDropDetectionCount
+        if multiDropDetectionCount > 0:
+            results[csvresultfilename]["multiDropsPerMultiDropDetection"] = multiDropCount / multiDropDetectionCount
+        results[csvresultfilename]["DropTruePositives"] = dropDetectionCount
+        results[csvresultfilename]["DropFalseNegatives"] = corruptionCount - dropDetectionCount
+        results[csvresultfilename]["DropRecall"] = dropDetectionCount / (dropDetectionCount + (corruptionCount - dropDetectionCount))
+        results[csvresultfilename]["LossTruePositives"] = lossDetectionCount
+        results[csvresultfilename]["LossFalseNegatives"] = corruptionCount - lossDetectionCount
+        results[csvresultfilename]["LossRecall"] = lossDetectionCount / (lossDetectionCount + (corruptionCount - lossDetectionCount))
+        results[csvresultfilename]["CombiTruePositives"] = combiDetectionCount
+        results[csvresultfilename]["CombiFalseNegatives"] = corruptionCount - combiDetectionCount
+        results[csvresultfilename]["CombiRecall"] = combiDetectionCount / (combiDetectionCount + (corruptionCount - combiDetectionCount))
     i += 1
     bar.update(i)
 bar.finish()
 
 print("Write results:")
 i = 0
-bar = progressbar.ProgressBar(max_value=len(results)*2, redirect_stdout=True).start()
+bar = progressbar.ProgressBar(max_value=len(results)*4, redirect_stdout=True).start()
 time.sleep(1)
 file = open("./examples/tsn/filter_network/analysis_results_" + time.strftime("%Y%m%d-%H%M%S") + ".txt", "w")
+file.write("### Drop Detection Performance Table ###\n")
 file.write("{:<40} {:<20} {:<20} {:<20} {:<20}\n".format('simulation', 'corruptions', 'TruePositives', 'FalseNegatives', 'Recall'))
 for resultkey in results:
-    file.write("{:<40} {:<20} {:<20} {:<20} {:<20}".format(resultkey, results[resultkey]["corruptions"], results[resultkey]["TruePositives"], results[resultkey]["FalseNegatives"], results[resultkey]["Recall"]))
-    file.write("  ->  " + str(results[resultkey]["TruePositives"]) + " / " + str(results[resultkey]["FalseNegatives"]) + " / " + str(round(results[resultkey]["Recall"], 2)) + "\n")
+    file.write("{:<40} {:<20} {:<20} {:<20} {:<20}".format(resultkey, results[resultkey]["corruptions"], results[resultkey]["DropTruePositives"], results[resultkey]["DropFalseNegatives"], results[resultkey]["DropRecall"]))
+    file.write("  ->  " + str(results[resultkey]["DropTruePositives"]) + " / " + str(results[resultkey]["DropFalseNegatives"]) + " / " + str(round(results[resultkey]["DropRecall"], 2)) + "\n")
     i += 1
     bar.update(i)
 file.write("\n")
+file.write("### Loss Detection Performance Table ###\n")
+file.write("{:<40} {:<20} {:<20} {:<20} {:<20}\n".format('simulation', 'corruptions', 'TruePositives', 'FalseNegatives', 'Recall'))
+for resultkey in results:
+    file.write("{:<40} {:<20} {:<20} {:<20} {:<20}".format(resultkey, results[resultkey]["corruptions"], results[resultkey]["LossTruePositives"], results[resultkey]["LossFalseNegatives"], results[resultkey]["LossRecall"]))
+    file.write("  ->  " + str(results[resultkey]["LossTruePositives"]) + " / " + str(results[resultkey]["LossFalseNegatives"]) + " / " + str(round(results[resultkey]["LossRecall"], 2)) + "\n")
+    i += 1
+    bar.update(i)
+file.write("\n")
+file.write("### Combi Detection Performance Table (Drops + TDMA Loss) ###\n")
+file.write("{:<40} {:<20} {:<20} {:<20} {:<20}\n".format('simulation', 'corruptions', 'TruePositives', 'FalseNegatives', 'Recall'))
+for resultkey in results:
+    file.write("{:<40} {:<20} {:<20} {:<20} {:<20}".format(resultkey, results[resultkey]["corruptions"], results[resultkey]["CombiTruePositives"], results[resultkey]["CombiFalseNegatives"], results[resultkey]["CombiRecall"]))
+    file.write("  ->  " + str(results[resultkey]["CombiTruePositives"]) + " / " + str(results[resultkey]["CombiFalseNegatives"]) + " / " + str(round(results[resultkey]["CombiRecall"], 2)) + "\n")
+    i += 1
+    bar.update(i)
+file.write("\n")
+file.write("### Details ###\n")
 for resultkey in results:
     file.write(resultkey + " -> " + str(results[resultkey]) + "\n")
     i += 1
